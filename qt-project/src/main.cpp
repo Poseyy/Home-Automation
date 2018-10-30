784daf2f59b5a58efde020a66de9e073657e9b1e
Jacob Dorpinghaus <jacob.dorpinghaus@gmail.com>
2018-10-30 11:40:03 -0500

    remove old qt-project

--- qt-project/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include <QQmlApplicationEngine>
-
-int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
-    QQmlApplicationEngine appEngine(QUrl("qrc:/main.qml"));
-    return app.exec();
-}
